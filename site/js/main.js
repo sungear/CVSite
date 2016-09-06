09091c35273301ab33974564d31530143dd161c3
Sunpreet Singh <s.singh@form.willow.if3>
2016-09-06 13:04:08 +0200

    <nav> décollé du haut de la page

---
+++ site/js/main.js
@@ -0,0 +1,16 @@
+//Keep footer to the page bottom
+function footerAlign() {
+  $('footer').css('display', 'block');
+  $('footer').css('height', 'auto');
+  var footerHeight = $('footer').outerHeight();
+  $('body').css('padding-bottom', footerHeight);
+  $('footer').css('height', footerHeight);
+}
+
+$(document).ready(function(){
+  footerAlign();
+});
+
+$( window ).resize(function() {
+  footerAlign();
+});
